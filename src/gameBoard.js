bd4883cfb7f38d0e59fa63c5512dc898d3149b4a
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-05-29 16:17:01 -0400

    feat: Complete addShip() and add tests
    - addShip(x, y) adds a new ship to the board with the given parameters
    and ensures that it is in bounds. It uses constants

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -8,7 +8,7 @@ export class GameBoard {
         this.playerBoard = [];
         this.attackBoard = [];
         this.#initializeBoard();
-        this.ships = [];
+        this.playerShips = [];
     }
 
     #initializeBoard() {
@@ -34,7 +34,63 @@ export class GameBoard {
     }
 
     addShip(x, y, orientation, shipType) {
-        this.ships.push(new Ship(x, y, orientation, shipType));
+        if(this.cellOutOfBounds(x, y)) return "OUT OF BOUNDS";
+
+        let shipCells = [];
+
+        shipCells.push(this.getPlayerCell(x, y));
+
+        for(let i = 0; i < shipType.size; i++) {
+            if(orientation === CONST.HORIZONTAL) {
+                //Add cells moving to the right
+                let shipCellX = x + i; 
+
+                if(this.cellOutOfBounds(shipCellX, y)) return "OUT OF BOUNDS";
+
+                let newShipCell = this.getPlayerCell(shipCellX, y);
+
+                if(newShipCell.containsShip) return "OCCUPIED"; //Make sure there isn't a ship here already
+
+                shipCells.push(newShipCell);
+                
+            } else {
+                //Add cells moving to the right
+                let shipCellY = y + i; 
+
+                if(this.cellOutOfBounds(shipCellY, y)) return "OUT OF BOUNDS";
+
+                let newShipCell = this.getPlayerCell(x, shipCellY);
+
+                if(newShipCell.containsShip) return "OCCUPIED"; //Make sure there isn't a ship here already
+
+                shipCells.push(newShipCell);
+            }
+        }
+
+        let newShip = new Ship(x, y, orientation, shipType);
+
+        for(let i = 0; i < shipCells.length; i++) {
+            shipCells[i].containsShip = true;
+            newShip.cells.push(shipCells[i]);
+        }
+        
+        this.playerShips.push(newShip);
+    }
+
+    getPlayerCell(x, y) {
+        if(this.cellOutOfBounds(x, y) === true) throw new RangeError("Coordinates are out of bounds");
+
+        return this.playerBoard[x][y];
+    }
+
+    getAttackCell(x, y) {
+        if(this.cellOutOfBounds(x, y) === true) throw new RangeError("Coordinates are out of bounds");
+
+        return this.attackBoard[x][y];
+    }
+
+    cellOutOfBounds(x, y) {
+        return x < 0 || x > CONST.BOARD_SIZE - 1 || y < 0 || y > CONST.BOARD_SIZE - 1;
     }
 
     /* 
--- src/test/gameBoard.test.js
+++ src/test/gameBoard.test.js
@@ -6,12 +6,45 @@ describe("Game Board Testing", () => {
     let gameBoard = new GameBoard();
 
     describe('addShip()', () => {
-        it('Adds a new ship', () => {
+        it('Adds a new ship to the ship list', () => {
             gameBoard.addShip(1, 1, CONST.HORIZONTAL, SHIPS.BATTLESHIP);
 
-            expect(gameBoard.ships.length).toEqual(1);
-            expect(gameBoard.ships[0].x).toEqual(1);
-            expect(gameBoard.ships[0].name).toEqual(SHIPS.BATTLESHIP.name);
+            expect(gameBoard.playerShips.length).toEqual(1);
+            expect(gameBoard.playerShips[0].x).toEqual(1);
+            expect(gameBoard.playerShips[0].name).toEqual(SHIPS.BATTLESHIP.name);
+        });
+
+        it('Adds a new ship to the player board', () => {
+            gameBoard.addShip(1, 1, CONST.HORIZONTAL, SHIPS.BATTLESHIP);
+
+            expect(gameBoard.getPlayerCell(1, 1).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(2, 1).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(3, 1).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(4, 1).containsShip).toEqual(true);
+
+            gameBoard.addShip(2, 2, CONST.VERTICAL, SHIPS.BATTLESHIP);
+
+            expect(gameBoard.getPlayerCell(2, 2).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(2, 3).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(2, 4).containsShip).toEqual(true);
+            expect(gameBoard.getPlayerCell(2, 5).containsShip).toEqual(true);
+        })
+    });
+
+    describe('getPlayerCell()', () => {
+        it('Returns the correct cell', () => {
+            let cell = gameBoard.getPlayerCell(1, 9);
+
+            expect(cell.x).toEqual(1);
+            expect(cell.y).toEqual(9);
+        });
+
+        it('Throws when coordinate is out of bounds', () => {
+            let outOfBounds = () => {
+                gameBoard.getPlayerCell(10, 10);
+            }
+
+            expect(() => outOfBounds()).toThrow('Coordinates are out of bounds');
         });
     });
 
