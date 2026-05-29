a4b87c1780c2fb9a16ea9ae84bdd6bee3cbdc815
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-05-29 10:41:39 -0400

    feat: Add constants to files
    - General constants
    - File for ship types constants

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const HORIZONTAL = 0;
+export const VERTICAL = 1;
+export const BOARD_SIZE = 10;
---
+++ src/shipTypes.js
@@ -0,0 +1,22 @@
+export const SHIPS = {
+    CARRIER: {
+        name: "Carrier",
+        size: 5
+    },
+    BATTLESHIP: {
+        name: "Battleship",
+        size: 4
+    },
+    CRUISTER: {
+        name: "Cruiser",
+        size: 3
+    },
+    SUBMARINE: {
+        name: "Submarine",
+        size: 3
+    },
+    DESTROYER: {
+        name: "Destroyer", 
+        size: 2
+    }
+}
